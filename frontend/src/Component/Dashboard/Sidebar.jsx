3ce6919f4e6f97afd4d191234386663de20564eb
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-23 11:30:19 +0100

    update

--- frontend/src/Component/Dashboard/Sidebar.jsx
+++ frontend/src/Component/Dashboard/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Dashboard.css";
+import "./Dashboard.css"
 
 const dashboardLinks = [
   { title: "Students", url: "/studentdash" },
--- frontend/src/Component/Dashboard/StudentDashboard.jsx
+++ frontend/src/Component/Dashboard/StudentDashboard.jsx
@@ -1,6 +1,43 @@
 import React from 'react'
 import Sidebar from './Sidebar'
-import './Dashboard.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+
+const studentsData = [
+  {
+    id: 1,
+    name: "Jessica Smith",
+    email: "jessica.smith@gmail.com",
+    idNumber: "12345",
+    gender: "Female",
+    age: 20,
+    nationality: "American",
+  },
+  {
+    id: 2,
+    name: "Martins Jordan",
+    email: "Martins.Jordan@gmail.com",
+    idNumber: "12345",
+    gender: "Male",
+    age: 25,
+    nationality: "British",
+  },
+  {
+    id: 3,
+    name: "Monica Smith",
+    email: "Monica.Smith@gmail.com",
+    idNumber: "12345",
+    gender: "Female",
+    age: 30,
+    nationality: "Spanish",
+  },
+]
+
+
+
 
 const StudentDashboard = () => {
     
