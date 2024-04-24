b3a431e27ed667ab724b7d2bfdb16cad2d1bae81
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-24 15:45:39 +0100

    update

--- frontend/src/Component/AdminPreview/AdminPreview.jsx
+++ frontend/src/Component/AdminPreview/AdminPreview.jsx
@@ -4,7 +4,7 @@ import { CiSearch } from "react-icons/ci";
 import UserTable from "./UserTable";
 
 const userData = [
-  { name: "Nafisat", email: "john@example.com", role: "Admin", id: 1 },
+  { name: "Kenny", email: "john@example.com", role: "Admin", id: 1 },
   { name: "Zainab ", email: "Zainab@example.com", role: "User", id: 2 },
   { name: "Basirat", email: "Basirat@example.com", role: "Member", id: 3 },
   { name: "Azeez", email: "Azeez@example.com", role: "Admin", id: 4 },
@@ -47,6 +47,31 @@ const AdminPreview = () => {
     );
     setFilteredData(updatedFilteredData);
   };
+
+  return (
+    <div className="__prevCon">
+      <h2 className="__prevHeader">Admins</h2>
+
+      <div className="__prevSearchCon">
+        <CiSearch className="__prevSearchIcon" />
+        <input
+          type="text"
+          className="__prevSearch"
+          placeholder="Search by name or email or role"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+
+      <div className="__prevList">
+        <UserTable data={filteredData} onDelete={handleDelete}  onUpdateRole={handleUpdateRole}  />
+      </div>
+
+      <div className="__inviteBtnCon">
+        <button className="__inviteBtn">Invite Admin</button>
+      </div>
+    </div>
+  );
   
 
 
