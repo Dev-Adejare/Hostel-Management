5369693dea763843e0f96a56bc90bdb4ab0a12b8
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-24 11:09:49 +0100

    update

--- frontend/src/Component/Dashboard/EditStatusModal.jsx
+++ frontend/src/Component/Dashboard/EditStatusModal.jsx
@@ -19,10 +19,14 @@ const EditStatusModal = ({ room, onUpdateRoom, onClose }) => {
         <h2 className="modal-title">Edit Room Status</h2>
         <p className="room-number">Room Number: {room.roomNumber}</p>
         <label htmlFor="status" className="status-label">New Status:</label>
-        <div className="right">
 
-        <input type="text" id="status" className="search" value={newStatus} onChange={handleStatusChange} />
+        
+        <div className="right">
+        <input type="text" id="status" className="search" value={newStatus} 
+        onChange={handleStatusChange} />
         </div>
+       
+       
         <div className="button-group">
           <button className="save-button" onClick={handleSubmit}>Save</button>
           <button className="cancel-button" onClick={onClose}>Cancel</button>
