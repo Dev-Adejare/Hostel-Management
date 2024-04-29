43f43a571007815eac054d400d972614b04155ec
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-29 10:33:24 +0100

    update

---
+++ frontend/src/Component/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='--100vh --flex-center'>
+      <div id='loader'></div>
+    </div>
+  )
+}
+
+export default Loader
