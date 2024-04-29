148c767970c5fcde34ada62f22e0858639dbf6d6
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-29 11:29:08 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AdminReg from "./Component/Register/AdminReg";
 import Login from "./Component/Register/Login";
 import StudentReg from "./Component/Register/StudentReg";
@@ -8,43 +8,53 @@ import StudentDashboard from "./Component/Dashboard/StudentDashboard";
 import Rooms from "./Component/Dashboard/Rooms";
 import AdminPreview from "./Component/AdminPreview/AdminPreview";
 import Attendance from "./Component/Attendance/Attendance";
-import Loader from "./Component/Loader/Loader"
-import { useEffect } from "react";
-
+import Loader from "./Component/Loader/Loader";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
-  const [loading, setLoading] = usestate(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
-  useEffect(() =>{
-    setTimeout(() =>{
-      setLoading(false)
-    },3000)
-  }, [])
+  const renderRoute = () => (
+    <Routes>
+      <Route path="/" element={<AdminReg />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/student-reg"
+        element={
+          <Layout>
+            <StudentReg />
+          </Layout>
+        }
+      />
+      <Route
+        path="/homedash"
+        element={
+          <Layout>
+            <HomeDash />
+          </Layout>
+        }
+      />
+      <Route path="/studentdash" element={<StudentDashboard />} />
+      <Route path="/room" element={<Rooms />} />
+      <Route path="/adminsPrev" element={<AdminPreview />} />
+      <Route
+        path="/attendance"
+        element={
+          <Layout>
+            <Attendance />
+          </Layout>
+        }
+      />
+    </Routes>
+  );
 
-  
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<AdminReg/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/student-reg" element={<Layout><StudentReg/></Layout>}/>
-        <Route path="/homedash" element={<Layout><HomeDash/></Layout>}/>
-        <Route path="/studentdash" element={<StudentDashboard/>}/>
-        <Route path="/room" element={<Rooms />} />
-        <Route path="/adminsPrev" element={<AdminPreview/>}/>
-        <Route path="/attendance" element={<Layout><Attendance/></Layout>} />
-
-      </Routes>
-     
-    </div>
-  )
+  return <>{loading ? <Loader /> : renderRoute()}</>;
 }
 
-export default App
-        
-
-
-
-
-
+export default App;
