f529cae5b0354ad5eccc5879a4e32858681c1a02
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-30 16:44:44 +0100

    update

--- frontend/src/Component/Header/HeaderSideNav.jsx
+++ frontend/src/Component/Header/HeaderSideNav.jsx
@@ -1,21 +1,42 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
+const HeaderSideNav = ({ items, setNavToggle }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
+  // const handleLinkClick = (index) => {
+  //     setActiveIndex(index)
+  // }
 
-const HeaderSideNav = ({items, setNavToggle}) => {
-
-    const [activeIndex, setActiveIndex] = useState()
   return (
     <aside>
-        <div className="--flex-end --sidebar-close">
-            <FaTimes className="sidebar-toggle-icon" 
-            onClick={() => setNavToggle(false)}/>
-        </div>
-      
-    </aside>
-  )
-}
+      <div className="--flex-end --sidebar-close">
+        <FaTimes
+          className="sidebar-toggle-icon"
+          onClick={() => setNavToggle(false)}
+        />
+      </div>
 
-export default HeaderSideNav
+      <div className="left">
+        {items.map(({ title, url }, index) => (
+          <div className="--flex-center --dir-column" key={index}>
+            <Link
+              to={url}
+              className={index === activeIndex ? "active-link" : ""}
+              onClick={() => setActiveIndex(index)}
+            >
+              {title}
+            </Link>
+          </div>
+        ))}
+
+        <div className="--flex-start --flex-center">
+          <button className="btn-primary">New</button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default HeaderSideNav;
