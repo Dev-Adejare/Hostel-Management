ceabb6313271a2d5cbbf231678c64649c6503c89
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-23 12:52:32 +0100

    update

--- Backend/conrollers/adminController.js
+++ Backend/conrollers/adminController.js
@@ -1,4 +1,28 @@
-const asyncHandler = require('express-async-handler');
-const bcrypt = require('bcryptjs');
-const admin = require('../models/AdminModel');
-const {generateToken} = require ('../utilis/index');
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const admin = require("../models/AdminModel");
+const { generateToken } = require("../utilis/index");
+
+const register = asyncHandler(async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+
+    {
+      !fullname ||
+        !email ||
+        (!password &&
+          (() => {
+            res.status(400);
+            throw new Error("Please! fill all the required fields");
+          }));
+    }
+
+    {
+      password.lenght < 6 &&
+        (() => {
+          res.status(400);
+          throw new Error("Password must be up to 6 characters!");
+        });
+    }
+  } catch (error) {}
+});
