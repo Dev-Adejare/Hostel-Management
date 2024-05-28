ffa278a7c9291f8a91896511131ce2b93f2c6d36
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-28 15:51:09 +0100

    update

--- Backend/models/studentModel.js
+++ Backend/models/studentModel.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
 const guardianScheme = new mongoose.Scheme({
-    
+    guardianName: {
+        type: String,
+        required: true
+    },
+    guardianEmail: {
+        type: String,
+        required: [true, 'Please add a valid email address'],
+        trim: true,
+        unique: true
+    },
+    guardianPhone: {
+        type: Number,
+        required: true
+    },
+    guardianAddress: {
+        type: String,
+        required: true
+    },
+    guardianOccupation: {
+        type: String,
+        required: true
+    },
+    guardianRelation: {
+        type: String,
+        required: true
+    }
 })
