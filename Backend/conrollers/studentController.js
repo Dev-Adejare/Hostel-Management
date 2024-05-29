c1f3c1bd90710a184604905180f115578924f310
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-29 10:40:42 +0100

    update

--- Backend/conrollers/studentController.js
+++ Backend/conrollers/studentController.js
@@ -1,3 +1,50 @@
 const asyncHandler = require("express-async-handler");
-const student = require("../models/studentModel");
+const Student = require("../models/studentModel");
 const Room = require("../models/roomModel");
+const generateUniqueId = require("../utilis/generateUniqueId");
+
+
+const ensureUniqueId = async () => {
+    let uniqueId;
+    let idExists = true;
+
+    while(idExists) {
+        uniqueId = generateUniqueId();
+        const existingStudent = await Student.findById( uniqueId );
+        idExists = !!existingStudent;
+    }
+    return uniqueId;
+}
+
+// To Register student
+const registerStudent = asyncHandler (async(req, res) => {});
+
+//To get all Student
+const getAllStudents = asyncHandler (async(req, res) => {});
+
+//To get single Student
+const getStudent = asyncHandler (async(req, res) => {});
+
+//To update student profile
+const updateStudentProfile = asyncHandler (async(req, res) => {});
+
+//To change student Room
+const changeStudentRoom = asyncHandler (async(req, res) => {});
+
+//To update checkIn status
+const updateCheckInStatus = asyncHandler (async(req, res) => {});
+
+//To Delete Student
+const deleteStudent = asyncHandler (async(req, res) => {});
+
+module.exports = {
+    registerStudent,
+    getAllStudents,
+    getStudent,
+    updateStudentProfile,
+    changeStudentRoom,
+    updateCheckInStatus,
+    deleteStudent,
+    ensureUniqueId,
+}
+
