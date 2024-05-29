fcbcff4e97640413aef55bb99cb1069a23320e7e
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-29 09:23:14 +0100

    update

--- Backend/models/studentModel.js
+++ Backend/models/studentModel.js
@@ -1,60 +1,71 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const guardianSchema = new mongoose.Scheme({
-    guardianName: {
-        type: String,
-        required: true
-    },
-    guardianEmail: {
-        type: String,
-        required: [true, 'Please add a valid email address'],
-        trim: true,
-        unique: true
-    }
-})
+  guardianName: {
+    type: String,
+    required: true,
+  },
+  guardianEmail: {
+    type: String,
+    required: [true, "Please add a valid email address"],
+    trim: true,
+    unique: true,
+  },
+});
 
-const studentSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema(
+  {
     _id: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     age: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     gender: {
-        type: String,
-        required: true,
-        enum: ['male', 'female', 'others'],
+      type: String,
+      required: true,
+      enum: ["Male", "Female", "Others"],
     },
     nationality: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        required: [true, 'Please add a valid email address'],
-        trim: true,
-        unique: true
+    email: {
+      type: String,
+      required: [true, "Please add a valid email address"],
+      trim: true,
+      unique: true,
     },
-    guardian: guardianSchema, 
+    guardian: guardianSchema,
     room: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Room',
-        default: null,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      default: null,
     },
-    role:{
-        type: String,
-        default:'Student'
-    }
-
-
-
-
-})
-  
+    role: {
+      type: String,
+      enum: ["student"],
+      default: "student",
+    },
+    checkedIn: {
+      type: Boolean,
+      default: false,
+    },
+    checkedInTime: {
+      type: Date,
+      default: null,
+    },
+    checkedOutTime: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true, minimize: false, toJSON: { getters: flase } }
+);
