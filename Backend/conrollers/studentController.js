ecbe59e1855ac8a292f45c79fd16fbca677b0df7
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-29 11:10:00 +0100

    update

--- Backend/conrollers/studentController.js
+++ Backend/conrollers/studentController.js
@@ -3,63 +3,97 @@ const Student = require("../models/studentModel");
 const Room = require("../models/roomModel");
 const generateUniqueId = require("../utilis/generateUniqueId");
 
-
 const ensureUniqueId = async () => {
-    let uniqueId;
-    let idExists = true;
+  let uniqueId;
+  let idExists = true;
 
-    while(idExists) {
-        uniqueId = generateUniqueId();
-        const existingStudent = await Student.findById( uniqueId );
-        idExists = !!existingStudent;
-    }
-    return uniqueId;
-}
+  while (idExists) {
+    uniqueId = generateUniqueId();
+    const existingStudent = await Student.findById(uniqueId);
+    idExists = !!existingStudent;
+  }
+  return uniqueId;
+};
 
 // To Register student
-const registerStudent = asyncHandler (async(req, res) => {
-    const {name, age, gender, nationality, email, g_name, g_mail, roomNum} = req.body;
+const registerStudent = asyncHandler(async (req, res) => {
+  const { name, age, gender, nationality, email, g_name, g_mail, roomNum } =
+    req.body;
 
-    if (!name || !age || !gender || !nationality || !email || !g_name || !g_mail || roomNum) {
-        res.status(400);
-        throw new Error("Please! fill all the required fields");
-    };
+  if (
+    !name ||
+    !age ||
+    !gender ||
+    !nationality ||
+    !email ||
+    !g_name ||
+    !g_mail ||
+    roomNum
+  ) {
+    res.status(400);
+    throw new Error("Please! fill all the required fields");
+  }
 
-    //To check if Student is already Existing
+  //To check if Student is already Existing
+  const studentExists = await Student.find({ email });
 
-    const studentExists = await Student.find({ email });
+  if (studentExists) {
+    return res.status(400).json({ message: "Student already exists!" });
+  }
 
-    if (studentExists){
-        return res.status(400).json({message: "Student already exists!"})
-    } 
+  //To Check if Room is already Existing
+  const room = await Room.findOne({ roomNumber: roomNum });
+
+  if (!room) {
+    return res.status(400).json({ message: "Room not Found!" });
+  }
+  //To Check the availability of selected Room
+  if (room.roomStatus !== "available") {
+    return res.status(400).json({ message: "Room not available!" });
+  }
+
+  const uniqueId = ensureUniqueId;
+
+  const student = await Student.create({
+    _id: uniqueId,
+    name,
+    age,
+    nationality,
+    email,
+    guardian: {
+      guardianName: g_name,
+      guardianEmail: g_mail,
+    },
+    gender,
+    room: room._id,
+  });
 });
 
 //To get all Student
-const getAllStudents = asyncHandler (async(req, res) => {});
+const getAllStudents = asyncHandler(async (req, res) => {});
 
 //To get single Student
-const getStudent = asyncHandler (async(req, res) => {});
+const getStudent = asyncHandler(async (req, res) => {});
 
 //To update student profile
-const updateStudentProfile = asyncHandler (async(req, res) => {});
+const updateStudentProfile = asyncHandler(async (req, res) => {});
 
 //To change student Room
-const changeStudentRoom = asyncHandler (async(req, res) => {});
+const changeStudentRoom = asyncHandler(async (req, res) => {});
 
 //To update checkIn status
-const updateCheckInStatus = asyncHandler (async(req, res) => {});
+const updateCheckInStatus = asyncHandler(async (req, res) => {});
 
 //To Delete Student
-const deleteStudent = asyncHandler (async(req, res) => {});
+const deleteStudent = asyncHandler(async (req, res) => {});
 
 module.exports = {
-    registerStudent,
-    getAllStudents,
-    getStudent,
-    updateStudentProfile,
-    changeStudentRoom,
-    updateCheckInStatus,
-    deleteStudent,
-    ensureUniqueId,
-}
-
+  registerStudent,
+  getAllStudents,
+  getStudent,
+  updateStudentProfile,
+  changeStudentRoom,
+  updateCheckInStatus,
+  deleteStudent,
+  ensureUniqueId,
+};
--- Backend/models/roomModel.js
+++ Backend/models/roomModel.js
@@ -19,7 +19,7 @@ const roomSchema = new mongoose.Schema({
   },
   roomStatus: {
     type: String,
-    default: "Unavailable",
+    default: "available",
   },
 }, {timestamps: true });
 
