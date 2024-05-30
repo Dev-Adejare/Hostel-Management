8922d7b7eae8d89d2e94b5d3346d238fdf06cc19
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-30 15:04:09 +0100

    update

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const Admin = require("../models/AdminModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await Admin.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        res.status(401);
+        throw new Error("Not authorized, user not found");
+      }
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
