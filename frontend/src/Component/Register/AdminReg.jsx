3ace72a8ff832758a695e3aead474cd3a06bef9f
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-06-03 15:39:07 +0100

    update

--- frontend/src/Component/Register/AdminReg.jsx
+++ frontend/src/Component/Register/AdminReg.jsx
@@ -53,41 +53,44 @@ const AdminReg = () => {
     setPassLength(password.length > 5);
   }, [formData.password]);
 
-  const handleSubmit = useCallback((event) => {
-    event.preventDefault();
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
 
-    const { fullname, email, password, Password2 } = formData;
+      const { fullname, email, password, Password2 } = formData;
 
-    if (!fullname || !email || !password || !Password2) {
-      setFormValidMessage("oop!! All fields are required 😏");
-      return;
-    }
+      if (!fullname || !email || !password || !Password2) {
+        setFormValidMessage("oop!! All fields are required 😏");
+        return;
+      }
 
-    if (password !== Password2) {
-      setFormValidMessage("Passwords do not match");
-      return;
-    }
+      if (password !== Password2) {
+        setFormValidMessage("Passwords do not match");
+        return;
+      }
 
-    setIsSubmitting(true);
+      setIsSubmitting(true);
 
-    axios
-      .post("http:localhost:3500/admin/register", formData)
-      .then((response) => {
-        setUser(response.data);
-        setIsSubmitting(false);
-        setFormCompleted(true);
-        toast.success("Registration successful");
-        navigate("/homedash", { state: { user: response.data } });
-      })
-      .catch((error) => {
-        setIsSubmitting(false);
-        const message =
-          error.response?.status === 400
-            ? "A user with the same email already exists."
-            : "Server error, unable to register user.";
-        setFormValidMessage(message);
-      });
-  });
+      axios
+        .post("http:localhost:3500/admin/register", formData)
+        .then((response) => {
+          setUser(response.data);
+          setIsSubmitting(false);
+          setFormCompleted(true);
+          toast.success("Registration successful");
+          navigate("/homedash", { state: { user: response.data } });
+        })
+        .catch((error) => {
+          setIsSubmitting(false);
+          const message =
+            error.response?.status === 400
+              ? "A user with the same email already exists."
+              : "Server error, unable to register user.";
+          setFormValidMessage(message);
+        });
+    },
+    [formData, navigate, setUser]
+  );
 
   useEffect(() => {
     setTimeout(() => {
@@ -103,37 +106,57 @@ const AdminReg = () => {
         <div className="container form__ --100vh">
           <div className="form-container">
             <p className="title">Create an account</p>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
               <div className="--dir-column">
-                <label htmlFor="name">Full name:</label>
+                <label htmlFor="fullname">Full name:</label>
                 <input
                   type="text"
                   className="input"
-                  name="name"
+                  name="fullname"
                   placeholder="Enter your full name"
                   required
+                  value={formData.fullname}
+                  onChange={handleInputChange}
                 />
               </div>
 
               <div className="--dir-column">
-                <label htmlFor="name">Email:</label>
+                <label htmlFor="email">Email:</label>
                 <input
                   type="email"
                   className="input"
                   name="email"
                   placeholder="Enter your email"
                   required
+                  value={formData.email}
+                  onChange={handleInputChange}
                 />
               </div>
 
               <div className="--dir-column">
                 <label htmlFor="password">Password:</label>
-                <PasswordInput placeholder="password" name="password" />
+                <PasswordInput
+                  placeholder="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
               </div>
 
               <div className="--dir-column">
-                <label htmlFor="name">Confirm password:</label>
-                <PasswordInput placeholder="password" name="password" />
+                <label htmlFor="password2">Confirm password:</label>
+                <PasswordInput
+                  placeholder="password"
+                  name="password2"
+                  value={formData.Password2}
+                  onChange={handleInputChange}
+                  onPaste={(e) => {
+                    e.preventDefault();
+                    toast.error("Cannot paste into input field")
+                  }
+
+                  }
+                />
               </div>
               <button className="--btn">Create account</button>
             </form>
