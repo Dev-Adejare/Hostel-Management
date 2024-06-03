b97bf974376b3a654341f8132f6809d22e3a6836
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-06-03 11:10:07 +0100

    update

--- frontend/src/Component/PasswordInput/PasswordInput.jsx
+++ frontend/src/Component/PasswordInput/PasswordInput.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import "./PasswordInput.css"
+import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai"
 
-const PasswordInput = () => {
+const PasswordInput = ({placeholder, value, onChange, name, onPaste}) => {
+    const [showPassword, setShowPassword] = useState(false)
+    const togglePassword = () => {
+      setShowPassword(!showPassword)
+    }
   return (
     <div>
       
