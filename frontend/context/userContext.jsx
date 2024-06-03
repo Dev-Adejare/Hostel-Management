2f207a5ff37492457a689aea7ab43a358ad9d178
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-06-03 09:43:16 +0100

    update

--- frontend/context/userContext.jsx
+++ frontend/context/userContext.jsx
@@ -1,3 +1,24 @@
 import { createContext, useState, useEffect } from "react";
 
-export const usercontext = createContext();
+export const Usercontext = createContext();
+
+export const Userprovider = ({children}) => {
+    const [user, setUser] = useState(() => {
+        const savedUser = localStorage.getItem('user');
+        return savedUser? JSON.parse(savedUser) : null;
+    });
+
+    useEffect(() => {
+        if(user) {
+            localStorage.setItem('user', JSON.stringify(user));
+        }else {
+            localStorage.removeItem('user');
+        }
+    }, [user])
+
+    return (
+        <Usercontext.Provider value={{user, setUser}}>
+            {children}
+        </Usercontext.Provider>
+    )
+}
