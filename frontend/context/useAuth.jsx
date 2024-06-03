3c5e8e66266144d960f5ea0c3e4ce61a68ce799b
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-06-03 10:01:51 +0100

    update

--- frontend/context/useAuth.jsx
+++ frontend/context/useAuth.jsx
@@ -2,3 +2,15 @@ import { useEffect, useContext } from "react";
 import { useNavigate} from "react-router-dom";
 import { Usercontext } from "./userContext";
 
+const useAuthRedirect = () => {
+    const {user} = useContext(Usercontext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/login");
+        }
+    }, [user, navigate]);
+}
+
+export default useAuthRedirect;
